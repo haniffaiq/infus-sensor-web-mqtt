9b8435b0594d2b8b47b429e80d52ade24f0e5884
haniffaiq <haniffaiq95@gmail.com>
2022-11-08 16:11:31 +0700

    Commit get mqtt response

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 
-// import './App.css';
+import { React, useState, useEffect } from 'react'
+import axios from "axios";
 import CardSensor from "./components/CardSensor";
+const baseURL = "http://127.0.0.1:3001/sensor";
+
 function App() {
+  let [data, setdata] = useState([]);
+  let [sensor1, setsensor1] = useState('30');
+  let [sensor2, setsensor2] = useState('90');
+  
+  const getData = async() => {
+    try {
+      await axios.get(baseURL).then((response) => {
+        setdata(response.data['data']);
+        // console.log(data);
+        setsensor1(data['sensor1'])
+        setsensor2(data['sensor2'])
+        console.log(data);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    getData()
+    }, [sensor1]);
   return (
     <div className="App">
       <div class="flex flex-row mt-10">
 
         <div class="flex-none md:flex-1 ...">
-        <CardSensor title="Volume Infus" />
+        <CardSensor title="Volume Infus" data={sensor1}/>
         </div>
         <div class="flex-none md:flex-1 ...">
-        <CardSensor title="Kecepatan Cairan " />
+        <CardSensor title="Kecepatan Cairan " data={sensor2}/>
         </div>
 
       </div>
--- src/components/CardSensor/CardSensor.js
+++ src/components/CardSensor/CardSensor.js
@@ -4,9 +4,10 @@ import { React, useState, useEffect } from 'react'
 
 
 
-
 function CardSensor(props) {
 
+
+      
     return (
         <div>
             <div class="flex justify-center">
@@ -19,7 +20,7 @@ function CardSensor(props) {
                         <p class="text-gray-700 text-base mb-4">
                             Pada Card ini akan menampilkan informasi tentang <b>{props.title}</b>
                         </p>
-                        <p class="text-gray-900 text-5xl font-medium mb-2">100</p>
+                        <p class="text-gray-900 text-5xl font-medium mb-2">{props.data}</p>
                         {/* <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button> */}
                     </div>
                     <div class="py-3 px-6 border-t border-gray-300 text-gray-600">
